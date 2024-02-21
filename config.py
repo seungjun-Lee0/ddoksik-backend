f046538b76a18bd5fb9a5d4eca7e414d071e2a60
bangwol08 <bangwol08@naver.com>
2024-02-21 11:03:22 +0900

    update config file

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from boto3 import client
 
 APP_ENV = os.getenv('APP_ENV', 'development')
 DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'postgres')
@@ -6,3 +7,17 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'avocado1')
 DATABASE_HOST = os.getenv('DATABASE_HOST', 'ddoksik1.cricim2es6bi.ap-northeast-2.rds.amazonaws.com')
 DATABASE_NAME = os.getenv('DATABASE_NAME', 'ddoksik')
 TEST_DATABASE_NAME = os.getenv('DATABASE_NAME', 'test_health-care')
+
+COGNITO_USERPOOL_ID = "ap-northeast-2_zd2aqBRM4"
+COGNITO_CLIENT_ID = "684v4b4ei247ojk4pnqt88tapk"
+COGNITO_REGION = "ap-northeast-2"
+
+AWS_ACCESS_KEY_ID = "AKIAUXEEDCLZCWQ2A7KV"
+AWS_SECRET_ACCESS_KEY = "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"
+
+cognito_client = client(
+    'cognito-idp',
+    region_name=os.getenv('ap-northeast-2'),
+    aws_access_key_id=os.getenv('AKIAUXEEDCLZCWQ2A7KV'),
+    aws_secret_access_key=os.getenv('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')
+)
--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-# database.py
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 import config
@@ -13,10 +12,6 @@ Base = declarative_base()
 SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
-
-# async def create_tables():
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
 
 # Dependency to get async DB session
 async def get_db() -> AsyncSession:
